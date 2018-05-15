69c86a1d0d5c286d622ea8183694ee5a26e414df
Gabriel Dos Ramos <dosramosgabriel@gmail.com>
2018-05-15 23:17:45 +0200

    keywords support

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+keywords = [
+    "android"
+]
+
 box = "#content .box-holder > div.item"
 box_title = ".item-holder .item-frame .item-panel .entry-title a"
+box_link = ".item-holder .link-holder a"
 
 driver = webdriver.Firefox(executable_path="drivers/geckodriver")
 driver.get("https://udemycoupon.learnviral.com/coupon-category/free100-discount/")
 
 for item in driver.find_elements_by_css_selector(box):
     title = item.find_element_by_css_selector(box_title).text
-    print(title)
+    for key in keywords:
+        if key in title.lower():
+            print(title)
+            item.find_element_by_css_selector(box_link).click()
 
 driver.close()
