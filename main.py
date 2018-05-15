8fbfb742e5d386273f92f6a7a326ddcbb6750582
Gabriel Dos Ramos <dosramosgabriel@gmail.com>
2018-05-16 00:28:42 +0200

    localized button to enroll on the course

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -11,15 +12,34 @@ print(keywords)
 box = "#content .box-holder > div.item.status-publish"
 box_title = ".item-holder .item-frame .item-panel .entry-title a"
 box_link = ".item-holder .link-holder a"
+course = "div.buy-box button.course-cta[data-purpose=\"buy-this-course-button\"]"
 
 driver = webdriver.Firefox(executable_path="drivers/geckodriver")
 driver.get("https://udemycoupon.learnviral.com/coupon-category/free100-discount/")
+
+main_window = driver.current_window_handle
+
+def course_set(title, firefoxElement):
+    print(title)
+    firefoxElement.find_element_by_css_selector(box_link).click()
+
+    time.sleep(5)
+    link_window = driver.window_handles[1]
+    driver.switch_to_window(link_window)
+
+    try:
+        get_button = driver.find_element_by_css_selector(course)
+        print(get_button.text)
+    except Exception as e:
+        print(e)
+        
+    driver.close()
+    driver.switch_to_window(main_window)
 
 for item in driver.find_elements_by_css_selector(box):
     title = item.find_element_by_css_selector(box_title).text
     for key in keywords:
         if key in title.lower():
-            print(title)
-            item.find_element_by_css_selector(box_link).click()
+            course_set(title, item)
 
 driver.close()
