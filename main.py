a4ab234b5df8c670f0adcdbf78d475ac38a03742
Gabriel Dos Ramos <dosramosgabriel@gmail.com>
2018-05-17 00:25:43 +0200

    Added pagination search

--- main.py
+++ main.py
@@ -4,23 +4,25 @@ from selenium.webdriver.common.keys import Keys
 
 from src.udemy import Udemy
 
-def open_no_newline(path):
+# substitute with the udemy account data
+class User:
+    name = "usename"
+    password = "password"
+
+# number of pages to search
+pages = 5
+
+def lines(path):
     for line in open(path):
         yield line.replace("\r\n", "").replace("\n", "")
 
-keywords = [line for line in open_no_newline("keywords.txt")]
+keywords = [line for line in lines("keywords.txt")]
 print(keywords)
-
-user = "dosramosgabriel@gmail.com"
-password = "rustdev2018##"
 
 driver = webdriver.Firefox(executable_path="drivers/geckodriver")
 
 udemy = Udemy(driver, keywords)
-udemy.login("https://udemy.com", user, password)
-try:
-    udemy.extract_page_courses("https://udemycoupon.learnviral.com/coupon-category/free100-discount/")
-except Exception as e:
-    print(e)
+udemy.login("https://udemy.com", User.name, User.password)
+udemy.extract("https://udemycoupon.learnviral.com/coupon-category/free100-discount/", pages)
 
 driver.close()
--- src/udemy.py
+++ src/udemy.py
@@ -2,28 +2,44 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class CoursePage:
-    BUY_BUTTON = "div.buy-box button.course-cta[data-purpose=\"buy-this-course-button\"]"
+class Courses:
+    NEXT_PAGE = "#content div.content-box:nth-child(2) .paging .pages a.next"
 
-    def __init__(self, driver):
+    def __init__(self, driver, mainWindow, keywords):
         self.driver = driver
+        self.main = mainWindow
+        self.keywords = keywords
 
-    def focus(self):
-        self.driver.switch_to_window(self.driver.window_handles[1])
+    def extract(self, pages):
+        for i in range(1, pages + 1):
+            print("Courses of page ", i)
+            self.page_courses()
 
-    def get(self):
+            if not self.next_page(i, pages):
+                print("Not a next page")
+                break
+
+    def page_courses(self):
+        for course in self.driver.find_elements_by_css_selector(CourseBox.ELEMENT):
+            box = CourseBox(course)
+            if box.check(self.keywords):
+                box.get(self.driver)
+                self.driver.switch_to_window(self.main)
+
+    def next_page(self, current, limit):
         try:
-            button = self.driver.find_element_by_css_selector(CoursePage.BUY_BUTTON)
-            button.click()
-            time.sleep(5)
-        except Exception:
-            print("Course already purchased")
+            next = self.driver.find_element_by_css_selector(Courses.NEXT_PAGE)
+            if current < limit:
+                next.click()
+                time.sleep(5)
+        except Exception as e:
+            print("Pages finished ", e)
+            return False
 
-    def close(self):
-        self.driver.close()
+        return True
 
 class CourseBox:
-    SLEEP = 5
+    SLEEP = 7
     ELEMENT = "#content .box-holder > div.item.status-publish"
     TITLE = ".item-holder .item-frame .item-panel .entry-title a"
     LINK = ".item-holder .link-holder a"
@@ -48,6 +64,26 @@ class CourseBox:
         page.focus()
         page.get()
         page.close()
+
+class CoursePage:
+    BUY_BUTTON = "div.buy-box button.course-cta[data-purpose=\"buy-this-course-button\"]"
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def focus(self):
+        self.driver.switch_to_window(self.driver.window_handles[1])
+
+    def get(self):
+        try:
+            button = self.driver.find_element_by_css_selector(CoursePage.BUY_BUTTON)
+            button.click()
+            time.sleep(5)
+        except Exception:
+            print("Course already purchased")
+
+    def close(self):
+        self.driver.close()
 
 class Session:
     USER = "#login-form #id_email"
@@ -91,12 +127,7 @@ class Udemy:
             print("Unable to login", e)
             exit(1)
 
-    def extract_page_courses(self, address):
+    def extract(self, address, pages):
         self.driver.get(address)
         time.sleep(5)
-
-        for course in self.driver.find_elements_by_css_selector(CourseBox.ELEMENT):
-            box = CourseBox(course)
-            if box.check(self.keywords):
-                box.get(self.driver)
-                self.driver.switch_to_window(self.main)
+        Courses(self.driver, self.main, self.keywords).extract(pages)
