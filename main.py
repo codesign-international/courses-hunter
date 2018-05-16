9bb8c52ceb0818f3b41e624717822cad64a9cf6d
Gabriel Dos Ramos <dosramosgabriel@gmail.com>
2018-05-16 14:47:42 +0200

    Refactoring to improve modularity

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+from src.udemy import Udemy
+
 def open_no_newline(path):
     for line in open(path):
         yield line.replace("\r\n", "").replace("\n", "")
@@ -9,37 +11,10 @@ def open_no_newline(path):
 keywords = [line for line in open_no_newline("keywords.txt")]
 print(keywords)
 
-box = "#content .box-holder > div.item.status-publish"
-box_title = ".item-holder .item-frame .item-panel .entry-title a"
-box_link = ".item-holder .link-holder a"
-course = "div.buy-box button.course-cta[data-purpose=\"buy-this-course-button\"]"
-
 driver = webdriver.Firefox(executable_path="drivers/geckodriver")
 driver.get("https://udemycoupon.learnviral.com/coupon-category/free100-discount/")
 
-main_window = driver.current_window_handle
-
-def course_set(title, firefoxElement):
-    print(title)
-    firefoxElement.find_element_by_css_selector(box_link).click()
-
-    time.sleep(5)
-    link_window = driver.window_handles[1]
-    driver.switch_to_window(link_window)
-
-    try:
-        get_button = driver.find_element_by_css_selector(course)
-        print(get_button.text)
-    except Exception as e:
-        print(e)
-        
-    driver.close()
-    driver.switch_to_window(main_window)
-
-for item in driver.find_elements_by_css_selector(box):
-    title = item.find_element_by_css_selector(box_title).text
-    for key in keywords:
-        if key in title.lower():
-            course_set(title, item)
+udemy = Udemy(driver, keywords)
+udemy.extract_page_courses()
 
 driver.close()
---
+++ src/udemy.py
@@ -0,0 +1,61 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class CoursePage:
+    BUY_BUTTON = "div.buy-box button.course-cta[data-purpose=\"buy-this-course-button\"]"
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def focus(self):
+        self.driver.switch_to_window(self.driver.window_handles[1])
+
+    def get(self):
+        try:
+            button = self.driver.find_element_by_css_selector(CoursePage.BUY_BUTTON)
+            print(button.text)
+        except Exception as e:
+            print(e)
+
+    def close(self):
+        self.driver.close()
+
+class CourseBox:
+    SLEEP = 5
+    ELEMENT = "#content .box-holder > div.item.status-publish"
+    TITLE = ".item-holder .item-frame .item-panel .entry-title a"
+    LINK = ".item-holder .link-holder a"
+
+    def __init__(self, course):
+        self.course = course
+
+    def check(self, keywords):
+        title = self.course.find_element_by_css_selector(CourseBox.TITLE).text
+        for key in keywords:
+            if key in title.lower():
+                print(title)
+                return True
+
+        return False
+
+    def get(self, driver):
+        self.course.find_element_by_css_selector(CourseBox.LINK).click()
+        time.sleep(CourseBox.SLEEP)
+        page = CoursePage(driver)
+        page.focus()
+        page.get()
+        page.close()
+
+class Udemy:
+    def __init__(self, driver, keywords):
+        self.keywords = keywords
+        self.driver = driver
+        self.main = self.driver.current_window_handle
+
+    def extract_page_courses(self):
+        for course in self.driver.find_elements_by_css_selector(CourseBox.ELEMENT):
+            box = CourseBox(course)
+            if box.check(self.keywords):
+                box.get(self.driver)
+                self.driver.switch_to_window(self.main)
