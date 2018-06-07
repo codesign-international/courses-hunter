afed7e9ec8b1978919dd1a361aeabfee57bac000
Gabriel Dos Ramos <dosramosgabriel@gmail.com>
2018-06-07 15:06:09 +0200

    Splitted the code and added documentations comments

---
+++ src/courses.py
@@ -0,0 +1,241 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class Courses:
+    """ Base class to gestionate the website for the course coupons
+
+    Public Properties:
+        driver (WebDriver): the driver to use
+        main (Window handle): The main window
+        keywords (String Array): list of keywords to match
+
+    Public Methods:
+        extract (extracts the udemy coupons present on the url)
+    """
+
+    NEXT_PAGE = "#content div.content-box:nth-child(2) .paging .pages a.next"
+
+    def __init__(self, driver, main, keywords):
+        """ Creates a new Courses instance
+
+        Args:
+            driver (WebDriver): the driver to use
+            main (Window handle): The main window
+            keywords (String Array): list of keywords to match
+
+        Returns:
+            new Courses
+
+        Raises:
+            None
+        """
+
+        self.driver = driver
+        self.main = main
+        self.keywords = keywords
+
+    def extract(self, pages):
+        """ Extracts all the courses present that match a keyword for n pages
+
+        Args:
+            pages (int): Number of pages to verify
+
+        Returns:
+            None
+
+        Raises:
+            None
+        """
+
+        for i in range(1, pages + 1):
+            print("Courses of page ", i)
+            self.page_courses()
+
+            if not self.next_page(i, pages):
+                print("Not a next page")
+                break
+
+    def page_courses(self):
+        """ Extracts all the courses present on a single page
+
+            Returns:
+                None
+
+            Raises:
+                None
+        """
+
+        for course in self.driver.find_elements_by_css_selector(CourseBox.ELEMENT):
+            box = CourseBox(self.driver, course)
+            if box.check(self.keywords):
+                box.get()
+                self.driver.switch_to_window(self.main)
+
+    def next_page(self, current, limit):
+        """ Checks if there is a next page
+
+            Args:
+                current (int): Current page
+                limit (int): Last permitted page
+
+            Returns:
+                bool
+
+            Raises:
+                None
+        """
+
+        try:
+            next = self.driver.find_element_by_css_selector(Courses.NEXT_PAGE)
+            if current < limit:
+                next.click()
+                time.sleep(5)
+        except Exception as e:
+            print("Pages finished ", e)
+            return False
+
+        return True
+
+class CourseBox:
+    """ Class to represent the div containing the course
+
+    Public Properties:
+        driver (WebDriver): the driver to use
+        course (CourseBoxHTML Element): The HTML element representing the course
+
+    Public Methods:
+        check (checks if the course matches the keywords)
+        get (gets the course for the udemy account)
+    """
+
+    SLEEP = 7
+    ELEMENT = "#content .box-holder > div.item.status-publish"
+    TITLE = ".item-holder .item-frame .item-panel .entry-title a"
+    LINK = ".item-holder .link-holder a"
+
+    def __init__(self, driver, course):
+        """ Creates a new instance of CourseBox
+
+            Args:
+                driver (WebDriver): the driver to use
+                course (CourseBoxHTML Element): The HTML element representing the course
+
+            Returns:
+                new CourseBox
+
+            Raises:
+                None
+        """
+
+        self.course = course
+        self.driver = driver
+
+    def check(self, keywords):
+        """ Checks the if the course name matches any keyword
+
+            Args:
+                keywords (String Array): List of keywords to match
+
+            Returns:
+                bool
+
+            Raises:
+                None
+        """
+
+        title = self.course.find_element_by_css_selector(CourseBox.TITLE).text
+        print(title)
+        for key in keywords:
+            if key in title.lower().split():
+                print("Course passed")
+                return True
+
+        return False
+
+    def get(self):
+        """ Adds the course to the list of courses on Udemy
+
+            Returns:
+                None
+
+            Raises:
+                None
+        """
+
+        self.course.find_element_by_css_selector(CourseBox.LINK).click()
+        time.sleep(CourseBox.SLEEP)
+        page = CoursePage(self.driver)
+        page.focus()
+        page.get()
+        page.close()
+
+class CoursePage:
+    """ Class to represent the course page on udemy after clicking on the div
+
+    Public Properties:
+        driver (WebDriver): the driver to use
+
+    Public Methods:
+        focus (focus the driver on the course page)
+        get (adds the course to the udemy account)
+        close (closes the window)
+    """
+
+    BUY_BUTTON = "div.buy-box button.course-cta[data-purpose=\"buy-this-course-button\"]"
+
+    def __init__(self, driver):
+        """ Creates a new instance of CoursePage
+
+            Args:
+                driver (WebDriver): the driver to use
+
+            Returns:
+                new CoursePage
+
+            Raises:
+                None
+        """
+
+        self.driver = driver
+
+    def focus(self):
+        """ Sets the driver focus on the new page
+
+            Returns:
+                None
+
+            Raises:
+                None
+        """
+
+        self.driver.switch_to_window(self.driver.window_handles[1])
+
+    def get(self):
+        """ Adds the course to the udemy account
+
+            Returns:
+                None
+
+            Raises:
+                None
+        """
+
+        try:
+            button = self.driver.find_element_by_css_selector(CoursePage.BUY_BUTTON)
+            button.click()
+            time.sleep(5)
+        except Exception:
+            print("Course already purchased")
+
+    def close(self):
+        """ Closes the current course page
+
+            Returns:
+                None
+
+            Raises:
+                None
+        """
+
+        self.driver.close()
--- src/udemy.py
+++ src/udemy.py
@@ -1,101 +1,53 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
-class Courses:
-    NEXT_PAGE = "#content div.content-box:nth-child(2) .paging .pages a.next"
-
-    def __init__(self, driver, mainWindow, keywords):
-        self.driver = driver
-        self.main = mainWindow
-        self.keywords = keywords
-
-    def extract(self, pages):
-        for i in range(1, pages + 1):
-            print("Courses of page ", i)
-            self.page_courses()
-
-            if not self.next_page(i, pages):
-                print("Not a next page")
-                break
-
-    def page_courses(self):
-        for course in self.driver.find_elements_by_css_selector(CourseBox.ELEMENT):
-            box = CourseBox(course)
-            if box.check(self.keywords):
-                box.get(self.driver)
-                self.driver.switch_to_window(self.main)
-
-    def next_page(self, current, limit):
-        try:
-            next = self.driver.find_element_by_css_selector(Courses.NEXT_PAGE)
-            if current < limit:
-                next.click()
-                time.sleep(5)
-        except Exception as e:
-            print("Pages finished ", e)
-            return False
-
-        return True
-
-class CourseBox:
-    SLEEP = 7
-    ELEMENT = "#content .box-holder > div.item.status-publish"
-    TITLE = ".item-holder .item-frame .item-panel .entry-title a"
-    LINK = ".item-holder .link-holder a"
-
-    def __init__(self, course):
-        self.course = course
-
-    def check(self, keywords):
-        title = self.course.find_element_by_css_selector(CourseBox.TITLE).text
-        print(title)
-        for key in keywords:
-            if key in title.lower().split():
-                print("Course passed")
-                return True
-
-        return False
-
-    def get(self, driver):
-        self.course.find_element_by_css_selector(CourseBox.LINK).click()
-        time.sleep(CourseBox.SLEEP)
-        page = CoursePage(driver)
-        page.focus()
-        page.get()
-        page.close()
-
-class CoursePage:
-    BUY_BUTTON = "div.buy-box button.course-cta[data-purpose=\"buy-this-course-button\"]"
-
-    def __init__(self, driver):
-        self.driver = driver
-
-    def focus(self):
-        self.driver.switch_to_window(self.driver.window_handles[1])
-
-    def get(self):
-        try:
-            button = self.driver.find_element_by_css_selector(CoursePage.BUY_BUTTON)
-            button.click()
-            time.sleep(5)
-        except Exception:
-            print("Course already purchased")
-
-    def close(self):
-        self.driver.close()
+from .courses import Courses
 
 class Session:
+    """ Class for handling the udemy login
+
+    Public Properties:
+        driver (WebDriver): the driver to use
+        user (String): the user to put
+        password (String): the password to put
+
+    Public Methods:
+        login (efectuates the login)
+    """
+
     USER = "#login-form #id_email"
     PASSWORD = "#login-form #id_password"
     SUBMIT = "#login-form #submit-id-submit"
 
     def __init__(self, driver, user, password):
+        """ Creates a new instance of Session
+
+        Args:
+            driver (WebDriver): the driver to use
+            user (String): the udemy user
+            password (String): the udemy password
+
+        Returns:
+            new Session
+
+        Raises:
+            None
+        """
+
         self.driver = driver
         self.user = user
         self.password = password
 
     def login(self):
+        """ Makes the login on udemy
+
+        Returns:
+            None
+
+        Raises:
+            None
+        """
+
         user = self.driver.find_element_by_css_selector(Session.USER)
         user.clear()
         user.send_keys(self.user)
@@ -106,20 +58,72 @@ class Session:
         submit.click()
 
 class Udemy:
+    """ Base class for udemy related ops
+
+    Public Properties:
+        driver (WebDriver): the driver to use
+        main (Window handle): The main window
+        keywords (String Array): list of keywords to match
+
+    Public Methods:
+        login (efectuates the login on udemy url)
+        extract (extracts the udemy coupons present on the url)
+    """
+
     LOG_IN = "require-auth[data-purpose=header-login] a"
 
     def __init__(self, driver, keywords):
+        """ Creates a new instance of Udemy
+
+        Args:
+            driver (WebDriver): the driver to use
+            keywords (String Array): list of keywords to match
+
+        Returns:
+            new Udemy
+
+        Raises:
+            None
+        """
+
         self.keywords = keywords
         self.driver = driver
         self.main = self.driver.current_window_handle
 
     def login(self, address, user, password):
+        """ Makes the login on udemy
+
+        Args:
+            address (String): the udemy url
+            user (String): the udemy user
+            password (String): the udemy password
+
+        Returns:
+            None
+
+        Raises:
+            None
+        """
+
         self.driver.get(address)
         self.driver.find_element_by_css_selector(Udemy.LOG_IN).click()
         time.sleep(1)
         self.fill_form(user, password)
 
     def fill_form(self, user, password):
+        """ Fills the login form on udemy
+
+            Args:
+                user (String): the user to put
+                password (String): the password to put
+
+            Returns:
+                None
+
+            Raises:
+                None
+        """
+
         try:
             Session(self.driver, user, password).login()
             time.sleep(10)
@@ -128,6 +132,19 @@ class Udemy:
             exit(1)
 
     def extract(self, address, pages):
+        """ Extracts all the coupons that match a keyword for the udemy account
+
+        Args:
+            address (String): the url to the coupons website
+            pages (int): Number of pages to 
+
+        Returns:
+            None
+
+        Raises:
+            None
+        """
+
         self.driver.get(address)
         time.sleep(5)
         Courses(self.driver, self.main, self.keywords).extract(pages)
