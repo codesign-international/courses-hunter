08060d5d1894b0139c6b68e156746f97908b5f1c
Gabriel Dos Ramos Albo <dosramosgabriel@gmail.com>
2018-08-25 17:55:04 +0200

    Implementation of a small cli. Prioritize options in config.ini

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import plac
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -9,18 +10,113 @@ def lines(path):
     for line in open(path):
         yield line.replace("\r\n", "").replace("\n", "")
 
-config = ConfigParser()
-config.read("config.ini")
+confpath = "config.ini"
+keyspath = "keywords.txt"
+driverpath = "drivers/geckodriver"
 
-driver = webdriver.Firefox(executable_path="drivers/geckodriver")
-driver.implicitly_wait(2)
+class NoParser(Exception):
+    """
 
-udemy = Udemy(driver, [line for line in lines(config["Bot"]["keywords"])])
-udemy.login("https://udemy.com", config["Account"]["user"], config["Account"]["password"])
+    """
 
-udemy.extract(config["Bot"]["coupons"]) # on first extraction, get all the pages
-while True:
-    udemy.extract(config["Bot"]["coupons"], int(config["Bot"]["pages"]))
-    time.sleep(SLEEP_REDO)
+    pass
 
-driver.close()
+
+class Config:
+    """
+
+    """
+
+    def __init__(self):
+        """
+
+        """
+
+        self.parser = ConfigParser()
+
+    def parse(self, config):
+        """
+
+        """
+
+        if config is None:
+            self.parser = None
+            return
+
+        self.parser.read(config)
+
+    def pages(self, pages):
+        """
+
+        """
+
+        try:
+            if self.parser is None:
+                raise NoParser()
+
+            return int(self.parser["Bot"]["pages"])
+        except Exception:
+            return int(pages)
+
+    def keywords(self, keywords):
+        """
+
+        """
+
+        try:
+            if self.parser is None:
+                raise NoParser()
+
+            return str(self.parser["Bot"]["keywords"])
+        except Exception:
+            return str(keywords)
+
+    def driver(self, driver):
+        """
+
+        """
+
+        try:
+            if self.parser is None:
+                raise NoParser()
+
+            return str(self.parser["Bot"]["driver"])
+        except Exception:
+            return str(driver)
+
+    def account(self):
+        """
+
+        """
+
+        return (self.parser["Account"]["user"], self.parser["Account"]["password"])
+
+def parse(parser, pages, keywords, driverpath):
+    pages = parser.pages(pages)
+    keywords = parser.keywords(keywords)
+    driverpath = parser.driver(driverpath)
+
+    return (pages, keywords, driverpath)
+
+@plac.annotations(
+    config=plac.Annotation("Path to the config file", "option"),
+    pages=plac.Annotation("Number of pages to scan", "option"),
+    keywords=plac.Annotation("Path to the keywords file", "option"),
+    driverpath=plac.Annotation("Path to the web driver", "option")
+)
+def main(config=confpath, pages=5, keywords=keyspath, driverpath=driverpath):
+    parser = Config()
+    parser.parse(config)
+    pages, keywords, driverpath = parse(parser, pages, keywords, driverpath)
+    user, password = parser.account()
+
+    driver = webdriver.Firefox(executable_path=driverpath)
+    driver.implicitly_wait(2)
+
+    udemy = Udemy(driver, [line for line in lines(keywords)])
+    udemy.login("https://udemy.com", user, password)
+    udemy.extract(parser.parser["Bot"]["coupons"], pages)
+    driver.close()
+
+if __name__ == "__main__":
+    plac.call(main, version="0.2.0")
